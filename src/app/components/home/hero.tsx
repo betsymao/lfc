62c23aac316ead02a67cf172b2680a3384cf6392
Betsy <132311670+betsymao@users.noreply.github.com>
2024-06-29 21:54:10 +1000

    updated hero

--- src/app/components/home/hero.tsx
+++ src/app/components/home/hero.tsx
@@ -1,18 +1,75 @@
 "use client";
 
+import { Link } from "@chakra-ui/next-js";
+
 import {
   Box,
-  Stack,
-  Flex,
   Button,
+  Container,
+  Flex,
+  Heading,
+  HStack,
   Text,
   VStack,
-  useBreakpointValue,
 } from "@chakra-ui/react";
 
 export default function Hero() {
   return (
-    <Box></Box>
+    <Box
+      height={{ base: "50vh", md: "100vh" }}
+      backgroundImage={
+        "linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.1)),url('/hero.jpg')"
+      }
+      backgroundPosition={"bottom center"}
+      backgroundSize={"cover"}
+    >
+      <Container maxW="container.lg">
+        <VStack
+          color={"white"}
+          alignItems={{ base: "center", lg: "start" }}
+          justifyContent="center"
+          height={{ base: "50vh", md: "100vh" }}
+          spacing={{ base: "0.75rem", md: "2rem" }}
+        >
+          <Heading as="h1" size={{ base: "2xl", md: "3xl", lg: "4xl" }}>
+            Placeholder Heading
+          </Heading>
+          <Text
+            fontSize={{ base: "2xl", md: "3xl", lg: "4xl" }}
+            fontWeight={"semibold"}
+          >
+            Lorem ipsum dolor sit amet.
+          </Text>
+          <HStack spacing={{ base: 2, md: 4, lg: 6 }}>
+            <Button
+              as="a"
+              href="/about"
+              bgColor="yellow.500"
+              size={{ base: "md", md: "lg" }}
+              _hover={{
+                bgColor: "white",
+                textDecoration: "none",
+              }}
+            >
+              Learn more
+              {/* <span>icon</span> */}
+            </Button>
+            <Button
+              as="a"
+              href="/services"
+              bgColor="yellow.500"
+              size={{ base: "md", md: "lg" }}
+              _hover={{
+                bgColor: "white",
+                textDecoration: "none",
+              }}
+            >
+              View our services
+            </Button>
+          </HStack>
+        </VStack>
+      </Container>
+    </Box>
     // <Flex
     //   w={"full"}
     //   h={"100vh"}
