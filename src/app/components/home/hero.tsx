c3cce444a72bc9e8ecf144f44b3b743ed5384d4e
Betsy <132311670+betsymao@users.noreply.github.com>
2024-07-03 17:00:58 +1000

    updated hero

--- src/app/components/home/hero.tsx
+++ src/app/components/home/hero.tsx
@@ -9,117 +9,94 @@ import {
   Flex,
   Heading,
   HStack,
+  Icon,
   Text,
   VStack,
 } from "@chakra-ui/react";
 
+import { ChevronRightIcon } from "@chakra-ui/icons";
+
 export default function Hero() {
   return (
     <Box
+      className="hero"
       height={{ base: "50vh", md: "100vh" }}
-      backgroundImage={
-        "linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.1)),url('/hero.jpg')"
-      }
+      backgroundImage={"url('/hero.jpg')"}
       backgroundPosition={"bottom center"}
       backgroundSize={"cover"}
     >
       <Container maxW="container.lg">
         <VStack
           color={"white"}
-          alignItems={{ base: "center", lg: "start" }}
+          alignItems={"start"}
           justifyContent="center"
           height={{ base: "50vh", md: "100vh" }}
           spacing={{ base: "0.75rem", md: "2rem" }}
         >
-          <Heading as="h1" size={{ base: "2xl", md: "3xl", lg: "4xl" }}>
+          <Heading
+            className="hero__heading"
+            as="h1"
+            size={{ base: "2xl", md: "3xl", lg: "4xl" }}
+          >
             Placeholder Heading
           </Heading>
           <Text
+            className="hero__text"
             fontSize={{ base: "2xl", md: "3xl", lg: "4xl" }}
             fontWeight={"semibold"}
           >
             Lorem ipsum dolor sit amet.
           </Text>
-          <HStack spacing={{ base: 2, md: 4, lg: 6 }}>
+          <HStack spacing={{ base: 2, md: 4, lg: 6 }} wrap={"wrap"}>
             <Button
+              className="hero__cta"
               as="a"
               href="/about"
               bgColor="yellow.500"
-              size={{ base: "md", md: "lg" }}
+              border="1px"
+              borderColor="transparent"
+              color="white"
               _hover={{
-                bgColor: "white",
+                bgColor: "transparent",
+                borderColor: "yellow.500",
+                color: "yellow.500",
                 textDecoration: "none",
               }}
             >
-              Learn more
-              {/* <span>icon</span> */}
+              About us
+              <Icon
+                className="hero__cta-icon"
+                h={5}
+                w={5}
+                as={ChevronRightIcon}
+              />
             </Button>
             <Button
+              className="hero__cta"
               as="a"
               href="/services"
               bgColor="yellow.500"
-              size={{ base: "md", md: "lg" }}
+              border="1px"
+              borderColor="transparent"
+              color="white"
               _hover={{
-                bgColor: "white",
+                bgColor: "transparent",
+                borderColor: "yellow.500",
+                color: "yellow.500",
                 textDecoration: "none",
               }}
             >
               View our services
+              <Icon
+                className="hero__cta-icon"
+                h={5}
+                w={5}
+                as={ChevronRightIcon}
+              />
             </Button>
           </HStack>
         </VStack>
       </Container>
     </Box>
-    // <Flex
-    //   w={"full"}
-    //   h={"100vh"}
-    //   backgroundImage={
-    //     "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.1)), url('/hero.jpg')"
-    //   }
-    //   backgroundSize={"cover"}
-    //   backgroundPosition={"center center"}
-    // >
-    //   <VStack
-    //     w={"full"}
-    //     justify={"center"}
-    //     px={useBreakpointValue({ base: 4, md: 8 })}
-    //   >
-    //     <Stack maxW={"2xl"} align={"flex-start"} spacing={6}>
-    //       <Text
-    //         color={"white"}
-    //         fontWeight={700}
-    //         lineHeight={1.2}
-    //         fontSize={useBreakpointValue({ base: "3xl", md: "4xl" })}
-    //       >
-    //         Lorem ipsum dolor
-    //       </Text>
-    //       <Stack direction={"row"}>
-    //         <Button
-    //           border={"2px solid transparent"}
-    //           bg={"yellow.500"}
-    //           color={"black"}
-    //           rounded={"full"}
-    //           size="lg"
-    //           _hover={{
-    //             bg: "transparent",
-    //             border: "2px solid",
-    //             borderColor: "yellow.500",
-    //             color: "white",
-    //           }}
-    //         >
-    //           About Us
-    //         </Button>
-    //         {/* <Button
-    //           bg={"whiteAlpha.300"}
-    //           rounded={"full"}
-    //           color={"white"}
-    //           _hover={{ bg: "whiteAlpha.500" }}
-    //         >
-    //           Show me more
-    //         </Button> */}
-    //       </Stack>
-    //     </Stack>
-    //   </VStack>
-    // </Flex>
   );
 }
