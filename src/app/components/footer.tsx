a29686b63c54e308063b0e1e0184261cf40327e7
Betsy <132311670+betsymao@users.noreply.github.com>
2024-07-03 17:00:50 +1000

    updated footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -8,35 +8,37 @@ import {
   Box,
   Container,
   Flex,
+  Heading,
+  Icon,
   SimpleGrid,
   Stack,
   Text,
 } from "@chakra-ui/react";
 
+import { BsHouse, BsTelephone, BsEnvelope } from "react-icons/bs";
+
 const ListHeader = ({ children }: { children: ReactNode }) => {
   return (
-    <Text fontWeight={"600"} fontSize={"lg"} mb={2} color={"yellow.500"}>
+    <Heading as="h5" size="md" fontWeight={"600"} mb={2} color={"yellow.500"}>
       {children}
-    </Text>
+    </Heading>
   );
 };
 
 export function Footer() {
   return (
-    <Box bg={"black"} color={"white"}>
-      <Container
-        as={Stack}
-        // maxW="992px"
-        maxW="container.lg"
-        // px={{ base: 4, lg: 0 }}
-        pt={{ base: 4, md: 10 }}
-        pb={4}
-      >
+    <Box
+      bg={"black"}
+      color={"white"}
+      pt={{ base: 4, md: 10 }}
+      pb={{ base: 4, md: 0 }}
+    >
+      <Container as={Stack} maxW="container.lg">
         <SimpleGrid
           templateColumns={{
             sm: "1fr",
             md: "1fr 1fr",
-            lg: "1fr 2fr 2fr 2fr",
+            lg: "2fr 2fr 2fr 3fr",
           }}
           spacing={8}
         >
@@ -73,29 +75,24 @@ export function Footer() {
             <ListHeader>Mortgage Resources</ListHeader>
             <Box>
               <Link href="https://www.e-business.sro.vic.gov.au/calculators/land-transfer-duty">
-                Land Transfer (Stamp) Duty Calculator
+                Stamp Duty Calculator
               </Link>
             </Box>
           </Stack>
           <Stack align={"flex-start"}>
             <ListHeader>Contact</ListHeader>
-            <Box>
-              <Text>
-                {/* <Text as="b">Lawrence Mao</Text>
-                <br></br>
-                Director/Senior Mortgage Advisor
-                <br></br>
-                <br></br> */}
-                <Text as="b">Address:</Text> Suite 10/1 Main St,<br></br>
-                Box Hill VIC 3128
-                <br></br>
-                <br></br>
-                <Text as="b">Mobile:</Text> 0413 108 186
-                <br></br>
-                <br></br>
-                <Text as="b">Email:</Text> law@landmarkfinancial.com.au
-              </Text>
-            </Box>
+            <Flex alignItems={"center"} pb={2}>
+              <Icon as={BsHouse} mr={"4"} />
+              <Text>Suite 10/1 Main St, Box Hill VIC 3128</Text>
+            </Flex>
+            <Flex alignItems={"center"} pb={2}>
+              <Icon as={BsTelephone} mr={"4"} />
+              <Text>0413 108 186</Text>
+            </Flex>
+            <Flex alignItems={"center"}>
+              <Icon as={BsEnvelope} mr={"4"} />
+              <Text>law@landmarkfinancial.com.au</Text>
+            </Flex>
           </Stack>
         </SimpleGrid>
 
@@ -114,7 +111,7 @@ export function Footer() {
               ABN 1234567891234
             </Text>
           </Box>
-          <Box py={4}>
+          <Box py={{ base: 0, md: 4 }}>
             <Text fontSize={"sm"}>
               © 2024 Landmark Financial Corporation Pty Ltd.
             </Text>
