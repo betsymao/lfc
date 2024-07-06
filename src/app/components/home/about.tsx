921422ebfdd10926f887228f094a85b9ad6aad4c
Betsy <132311670+betsymao@users.noreply.github.com>
2024-07-06 14:54:55 +1000

    updated about

--- src/app/components/home/about.tsx
+++ src/app/components/home/about.tsx
@@ -4,6 +4,7 @@ import {
   Box,
   Button,
   Container,
+  Flex,
   Heading,
   SimpleGrid,
   Icon,
@@ -12,7 +13,8 @@ import {
   HStack,
   VStack,
 } from "@chakra-ui/react";
-import { CheckIcon } from "@chakra-ui/icons";
+
+import { ChevronRightIcon } from "@chakra-ui/icons";
 
 // Replace test data with your own
 const features = Array.apply(null, Array(4)).map(function (x, i) {
@@ -25,19 +27,56 @@ const features = Array.apply(null, Array(4)).map(function (x, i) {
 
 export default function About() {
   return (
-    <Box py={10}>
+    <Box
+      className="about"
+      height={"100%"}
+      backgroundImage={"url('/about.jpg')"}
+      backgroundPosition={"70% 35%"}
+      backgroundSize={"cover"}
+      py={{ base: 8, md: 24 }}
+    >
       <Container maxW={"container.lg"}>
-        <Box>
-          <Heading>Lorem ipsum dolor sit amet consectetur</Heading>
-          <Text>
+        <VStack
+          color={"white"}
+          alignItems={"start"}
+          justifyContent={"space-between"}
+          height={"100%"}
+          spacing={{ base: "1rem", md: "2rem" }}
+        >
+          <Heading className="about__heading">
+            Lorem ipsum dolor sit amet consectetur
+          </Heading>
+          <Text className="about__text">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex
             obcaecati quasi id consectetur? Ducimus omnis libero minima
             dignissimos id. Molestias tempora a, ipsam, tenetur quia sequi
             libero atque voluptatem et hic non, commodi ex doloribus sunt culpa
             deserunt voluptate laudantium.
           </Text>
-          <Button as="a">Learn more</Button>
-        </Box>
+          <Button
+            className="about__cta"
+            as="a"
+            href="/"
+            bgColor="yellow.500"
+            border="1px"
+            borderColor="transparent"
+            color="white"
+            _hover={{
+              bgColor: "transparent",
+              borderColor: "yellow.500",
+              color: "yellow.500",
+              textDecoration: "none",
+            }}
+          >
+            Learn more
+            <Icon
+              className="about__cta-icon"
+              h={5}
+              w={5}
+              as={ChevronRightIcon}
+            />
+          </Button>
+        </VStack>
       </Container>
     </Box>
   );
