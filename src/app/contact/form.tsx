6189f082ece7eafcee334c7bc6b40f9a81944ab9
Betsy <132311670+betsymao@users.noreply.github.com>
2024-08-06 16:58:50 +1000

    updated contact

---
+++ src/app/contact/form.tsx
@@ -0,0 +1,127 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+  Textarea,
+} from "@chakra-ui/react";
+
+type Inputs = {
+  first: string;
+  last: string;
+  email: string;
+  mobile: number;
+  message: string;
+};
+
+export default function HookForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl isInvalid={errors.first} className="contact__form-group">
+        <FormLabel htmlFor="first" className="contact__form-label">
+          First name
+        </FormLabel>
+        <Input
+          className="contact__form-input"
+          id="first"
+          type="text"
+          placeholder="John"
+          {...register("first", { required: "This field is required." })}
+        />
+        <FormErrorMessage>
+          {errors.first && errors.first.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <FormControl isInvalid={errors.last} className="contact__form-group">
+        <FormLabel htmlFor="last" className="contact__form-label">
+          Last name
+        </FormLabel>
+        <Input
+          className="contact__form-input"
+          id="last"
+          type="text"
+          placeholder="Doe"
+          {...register("last", { required: "This field is required." })}
+        />
+        <FormErrorMessage>
+          {errors.last && errors.last.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <FormControl isInvalid={errors.email} className="contact__form-group">
+        <FormLabel htmlFor="email" className="contact__form-label">
+          Email
+        </FormLabel>
+        <Input
+          className="contact__form-input"
+          id="email"
+          type="text"
+          placeholder="johndoe@email.com"
+          {...register("email", {
+            required: "This field is required.",
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Entered value does not match email format.",
+            },
+          })}
+        />
+        <FormErrorMessage>
+          {errors.email && errors.email.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <FormControl isInvalid={errors.mobile} className="contact__form-group">
+        <FormLabel htmlFor="mobile" className="contact__form-label">
+          Mobile number
+        </FormLabel>
+        <Input
+          className="contact__form-input"
+          id="mobile"
+          type="number"
+          placeholder="0123 456 789"
+          {...register("mobile", {
+            required: "This field is required.",
+            minLength: { value: 6, message: "Minimum length is 6." },
+            maxLength: { value: 12, message: "Maximum length is 12." },
+          })}
+        />
+        <FormErrorMessage>
+          {errors.mobile && errors.mobile.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <FormControl isInvalid={errors.message} className="contact__form-group">
+        <FormLabel htmlFor="message" className="contact__form-label">
+          Message
+        </FormLabel>
+        <Textarea
+          id="message"
+          type="text"
+          placeholder="Enter your message"
+          {...register("message", { required: "This field is required." })}
+        />
+        <FormErrorMessage>
+          {errors.last && errors.last.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <Button
+        isLoading={isSubmitting}
+        type="submit"
+        className="contact__form-btn"
+      >
+        Submit
+      </Button>
+    </form>
+  );
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,92 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import Form from "./form";
+import {
+  Container,
+  Flex,
+  Box,
+  Heading,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Textarea,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+
 export default function Contact() {
-  return <h1>Contact</h1>;
+  return (
+    <Flex className="contact" py={{ base: 8, md: 24 }}>
+      <Container maxWidth={"container.lg"}>
+        <Box maxW={{ base: "100%", lg: "50%" }}>
+          <Box pb={4}>
+            <Heading className="contact__heading">Contact</Heading>
+          </Box>
+          <Form />
+          {/* <form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl isInvalid={errors.name}>
+              <FormLabel htmlFor="name" className="contact__form-label">
+                First name
+              </FormLabel>
+              <Input
+                id="firstname"
+                className="contact__form-input"
+                type="text"
+                placeholder="John"
+                {...register("First name", {
+                  required: "This is required.",
+                  min: 1,
+                })}
+              />
+              <FormErrorMessage>
+                {errors.firstname && errors.firstname.message}
+              </FormErrorMessage>
+            </FormControl>
+
+            <FormLabel htmlFor="name" className="contact__form-label">
+              Last name
+            </FormLabel>
+            <Input
+              className="contact__form-input"
+              type="text"
+              placeholder="Doe"
+              {...register("Last name", {
+                required: "This is required.",
+                maxLength: 100,
+              })}
+            />
+
+            <FormLabel htmlFor="name" className="contact__form-label">
+              Email
+            </FormLabel>
+            <Input
+              className="contact__form-input"
+              type="email"
+              placeholder="johndoe@email.com"
+              {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+            />
+
+            <FormLabel htmlFor="name" className="contact__form-label">
+              Mobile number
+            </FormLabel>
+            <Input
+              className="contact__form-input"
+              type="tel"
+              placeholder="0123 456 789"
+              {...register("Mobile number", {
+                required: true,
+                minLength: 6,
+                maxLength: 12,
+              })}
+            />
+
+            <Input type="submit" className="contact__form-btn" />
+          </form> */}
+        </Box>
+      </Container>
+    </Flex>
+  );
 }
