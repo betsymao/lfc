7621a264a6683fc9ff87332d7ea9500f35a74d3a
Betsy <132311670+betsymao@users.noreply.github.com>
2024-08-06 16:57:59 +1000

    added about

---
+++ src/app/about/director.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { Container, Box, Heading } from "@chakra-ui/react";
+
+export default function Director() {
+  return <Box>Director</Box>;
+}
---
+++ src/app/about/overview.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { Container, Box, Heading } from "@chakra-ui/react";
+
+export default function Overview() {
+  return <Box>Overview</Box>;
+}
