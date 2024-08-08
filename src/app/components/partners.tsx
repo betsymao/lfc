cccfc3ee73cd33f1dcd86d22258ec871b40147ad
Betsy <132311670+betsymao@users.noreply.github.com>
2024-08-08 15:25:42 +1000

    updated partners

--- src/app/components/partners.tsx
+++ src/app/components/partners.tsx
@@ -4,43 +4,7 @@ import Script from "next/script";
 
 import Slider from "../js/slider";
 
-import {
-  Box,
-  Container,
-  Heading,
-  Text,
-  chakra,
-  SimpleGrid,
-  Stat,
-  StatLabel,
-  StatNumber,
-  useColorModeValue,
-} from "@chakra-ui/react";
-
-interface StatsCardProps {
-  title: string;
-  stat: string;
-}
-function StatsCard(props: StatsCardProps) {
-  const { title, stat } = props;
-  return (
-    <Stat
-      px={{ base: 4, md: 8 }}
-      py={"5"}
-      shadow={"xl"}
-      border={"1px solid"}
-      borderColor={useColorModeValue("gray.800", "gray.500")}
-      rounded={"lg"}
-    >
-      <StatLabel fontWeight={"medium"} isTruncated>
-        {title}
-      </StatLabel>
-      <StatNumber fontSize={"2xl"} fontWeight={"medium"}>
-        {stat}
-      </StatNumber>
-    </Stat>
-  );
-}
+import { Box, Container, Heading } from "@chakra-ui/react";
 
 export default function Partners() {
   return (
